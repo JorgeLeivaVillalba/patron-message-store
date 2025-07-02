f5c4991ef33a9102c420ae4843e6f37a1192abf6
Jorge <jorgeleiva.994@gmail.com>
2025-07-02 20:28:20 -0300

    final

--- src/main/java/com/example/MainApp.java
+++ src/main/java/com/example/MainApp.java
@@ -6,10 +6,11 @@ import org.h2.tools.Server;
 
 public class MainApp {
     public static void main(String[] args) throws Exception {
-        // Iniciar consola web de H2 en http://localhost:8082
+        // consola H2: http://localhost:8082
         Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start();
         Main main = new Main();
         main.configure().addRoutesBuilder(new MessageRoute());
         main.run();
     }
 }
+    
